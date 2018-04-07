5f07aec0fb86167157b81391d77fd232d8a5f23c
Steven Lagrange <slagrang@ualberta.ca>
2018-04-06 20:38:44 -0600

    Went through extensive refactoring.

--- static/js/functionUpdates.js
+++ static/js/functionUpdates.js
@@ -1,205 +1,251 @@
+$( document ).ready(function() {
+	console.log( "Loading functionUpdates.js!" );
+	//var api_url = "http://crawler-api.us-west-2.elasticbeanstalk.com/api/status/";
+	var apiURL = "http://localhost:3000/api/status/";
+	var requestFrequency = 0.2 / 1000;
+	var updateFrontEndFrequency = 0.2 / 1000;
 
-// drawing lines wheel to wheel making it look attached.
-var c = document.getElementById("drawlines");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.moveTo(15, 15);
-ctx.lineTo(190,15 );
-ctx.lineTo(150,15 );
-ctx.lineTo(150,140 );
-ctx.lineTo(15,140);
-ctx.lineTo(190,140);
-ctx.stroke();
+	var oldMessage = "";
+	var oldSteering = 0;
+	var state = 1;
 
-ctx.beginPath();
-ctx.moveTo(150, 15);
-ctx.lineTo(15,15);
-ctx.stroke();
+	var now = new Date();
+	var connectedMessage = "Connected.";
+	var offlineMessage = "Offline.";
+	var wheelColorSlipping = "#98bf21";
+	var wheelColorGood =  "#d9534f";
 
-ctx.beginPath();
-ctx.moveTo(150,140);
-ctx.lineTo(190,140);
-ctx.stroke();
+	var requestHandler;
+	var connectivityHandler;
+	var logHandler;
+	var steeringHandler;
+	var brakingHandler;
+	var sonarHandler;
+	var wheelFLHandler;
+	var wheelFRHandler;
+	var wheelRLHandler;
+	var wheelRRHandler;
 
-
-//ajax set up
-// The the url in the httpRequest.open is the crawler.com/api/get-crawler? then grab reponse text or obj (will change to
-// which ever
-
-//TODO make a button toggle to ask for
-
-//var ResponseTimer = setInterval(getCrawler, 1000);
-var crawler = {
-    "connected": 0,
-    "message": "Crawler not connected.",
-    "servo": 0,
-    "brake": 0,
-    "battery" : 0,
-    "sonar": 0,
-    "wheels": {
-      "fl": 0,
-      "fr": 0,
-      "rl": 0,
-      "rr": 0,
-		}
-  };
-
-function getCrawler(){
-
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function (){
-		//might be this.readyState == 4 && this.status == 200 for the real thing.
-		//change to this.status for checking dummy test
-		if (this.readyState == 4 && this.status == 200)
-			var response_data = JSON.parse(this.responseText);
-			crawler = response_data.crawler;
-		}
+	var crawler = {
+	    "connected": 0,
+	    "message": "Crawler not connected.",
+	    "steering": 0,
+	    "break": 0,
+	    "sonar": 0,
+	    "wheels": {
+	      "fl": 0,
+	      "fr": 0,
+	      "rl": 0,
+	      "rr": 0,
+			}
+	  };
 
 
-	//api_url = "http://crawler-api.us-west-2.elasticbeanstalk.com/api/status/"
-	api_url = "http://localhost:3000/api/status/"
-	xhttp.open("GET", api_url, true);
-	xhttp.send();
-}
-
-//Toggle the buttons to show, To see if we need to request or if we want to stop requesting
-function SwitchButtons(buttonId) {
-  var hideBtn, showBtn, menuToggle;
-  if (buttonId == 'batteryPosition') {
-    menuToggle = 'menu2';
-    showBtn = 'batteryPosition2';
-    hideBtn = 'batteryPosition';
-  } else {
-    menuToggle = 'menu3';
-    showBtn = 'batteryPosition';
-    hideBtn = 'batteryPosition2';
-  }
-  //I don't have your menus, so this is commented out.  just uncomment for your usage
-  // document.getElementById(menuToggle).toggle(); //step 1: toggle menu
-  document.getElementById(hideBtn).style.display = 'none'; //step 2 :additional feature hide button
-  document.getElementById(showBtn).style.display = ''; //step 3:additional feature show button
-
-
-}
-
-
-function changeConnectedStatus() {
-		if (connectedResponse == 1){
-			 document.getElementById("JSTRING").innerHTML = "connected";
-		}
-
-		else {
-			 document.getElementById("JSTRING").innerHTML = "offline";
-		}
-}
-
-
-// Time function, connection, command log, moving or breaking
-var myVar = setInterval(myTimer, 500);
-var oldCommand = "";
-function changeLogMessage() {
-    var d = new Date();
-
-    document.getElementById("time").innerHTML = d.toLocaleTimeString();
-
-    if(commandsResponse != oldCommand){
-        document.getElementById("loginfo").innerHTML += commandsResponse+ "<br />";
-        document.getElementById("loginfo").scrollTop =  document.getElementById("loginfo").scrollHeight;
-        oldCommand = commandsResponse;
-    }
+	drawWheels();
 
 
 
-
-
-}
-
-//Breaking animation
-var state = 1;
-function changeBreakingStatus() {
-	if(sonarResponse < 10){
-			state = 0;
-			document.getElementById("break").innerHTML = "Break";
-			document.getElementsByClassName('fa fa-cog fa-spin')[0].classList.toggle("fa-spin");
-
+	function run() {
+		console.log('Starting.');
+		requestHandler = setInterval(getCrawler, 1/requestFrequency);
+		connectivityHandler = setInterval(changeConnectedStatus, 1/updateFrontEndFrequency);
+		logHandler = setInterval(changeLogMessage, 1/updateFrontEndFrequency);
+		steeringHandler = setInterval(changeSteeringStatus, 1/updateFrontEndFrequency);
+		brakingHandler = setInterval(changeBrakingStatus, 1/updateFrontEndFrequency);
+		sonarHandler = setInterval(changeSonarStatus, 1/updateFrontEndFrequency);
+		wheelFLHandler = setInterval(changeWheelStatusFL, 1/updateFrontEndFrequency);
+		wheelFRHandler = setInterval(changeWheelStatusFR, 1/updateFrontEndFrequency);
+		wheelRLHandler = setInterval(changeWheelStatusRL, 1/updateFrontEndFrequency);
+		wheelRRHandler = setInterval(changeWheelStatusRR, 1/updateFrontEndFrequency);
 	}
 
-	else if(sonarResponse > 10 && state == 0){
-			state = 1;
-			document.getElementById("break").innerHTML = "Moving";
-			document.getElementsByClassName('fa fa-cog')[0].classList.toggle("fa-spin");
-
+	function end() {
+		console.log('Ending.');
+		clearInterval(requestHandler);
+		clearInterval(logHandler);
+		clearInterval(steeringHandler);
+		clearInterval(brakingHandler);
+		clearInterval(sonarHandler);
+		clearInterval(wheelFLHandler);
+		clearInterval(wheelFRHandler);
+		clearInterval(wheelRLHandler);
+		clearInterval(wheelRRHandler);
 	}
-}
+
+	$('#request').click(function() {
+		if ($(this).hasClass('running')) {
+			end();
+			$(this).text('Connect');
+			$(this).removeClass('running');
+			$(this).removeClass('btn-warning');
+			$(this).addClass('btn-success');
+		} else {
+			run();
+			$(this).text('Disconnect');
+			$(this).addClass('running');
+			$(this).addClass('btn-warning');
+			$(this).removeClass('btn-success');
+		}
+	});
+
+	function drawWheels() {
+		// Drawing lines wheel to wheel making it look attached.
+		var c = document.getElementById("wheels-canvas");
+		var ctx = c.getContext("2d");
+		ctx.beginPath();
+		ctx.moveTo(15, 15);
+		ctx.lineTo(190,15 );
+		ctx.lineTo(150,15 );
+		ctx.lineTo(150,140 );
+		ctx.lineTo(15,140);
+		ctx.lineTo(190,140);
+		ctx.stroke();
+
+		ctx.beginPath();
+		ctx.moveTo(150, 15);
+		ctx.lineTo(15,15);
+		ctx.stroke();
+
+		ctx.beginPath();
+		ctx.moveTo(150,140);
+		ctx.lineTo(190,140);
+		ctx.stroke();
+	}
 
 
+	function getCrawler(){
+		/*
+			AJAX request to Crawler API. Updates JSON Crawler variable.
+		*/
+		var xhttp = new XMLHttpRequest();
+		xhttp.open("GET", apiURL, true);
+		xhttp.send();
+
+		xhttp.onreadystatechange = function (){
+			if (this.readyState == 4 && this.status == 200)
+				var responseData = JSON.parse(this.responseText);
+				console.log(responseData);
+				crawler = responseData.crawler;
+			}
+	}
 
 
+	function changeConnectedStatus() {
+		/*
+			Updates the connectivity status on the front end of the website.
 
-//Steering Animation
-var updateSteering = setInterval(changeSteeringStatus(crawler.servo), 1000);
+			status = Int, used to indicate connectivty of crawler.
+		*/
+			if (crawler.connected == 1){
+				 document.getElementById("connectivity").innerHTML = connectedMessage;
+			}
+			else {
+				 document.getElementById("connectivity").innerHTML = offlineMessage;
+			}
+	}
 
-var old = 0;
-function changeSteeringStatus(servo){
 
-	if(old != servo){
-		if(servo  == 1 ){
-			document.getElementById("fl").style.transform = "rotate("+20+"deg)";
-			document.getElementById("fr").style.transform = "rotate("+20+"deg)";
-  		}
+	function changeLogMessage() {
+		/*
+			Updates Log Message on front end.
+		*/
+	  document.getElementById("time").innerHTML = now.toLocaleTimeString();
 
-    	else if(servo == 0){
-			document.getElementById("fl").style.transform = "rotate("+0+"deg)";
-			document.getElementById("fr").style.transform = "rotate("+0+"deg)";
-   		}
+	  if(crawler.message != oldMessage){
+	      document.getElementById("logger-message").innerHTML += crawler.message+ "<br />";
 
-    	else if(servo == -1 ){
-			document.getElementById("fl").style.transform = "rotate("+-20+"deg)";
-			document.getElementById("fr").style.transform = "rotate("+-20+"deg)";
+				/*
+	      document.getElementById("logg-message").scrollTop =  document.getElementById("logger-message").scrollHeight;
+				*/
+	      oldMessage = crawler.message;
+	  }
+	}
+
+
+	function changeSteeringStatus(){
+		/*
+			Animate steering from the Crawler.
+
+			steering - Int, latest steering status recieved.
+		*/
+		if(oldSteering != crawler.steering){
+			if(crawler.steering  == 1 ){
+				document.getElementById("fl").style.transform = "rotate("+20+"deg)";
+				document.getElementById("fr").style.transform = "rotate("+20+"deg)";
+	  		}
+
+	    	else if(crawler.steering == 0){
+				document.getElementById("fl").style.transform = "rotate("+0+"deg)";
+				document.getElementById("fr").style.transform = "rotate("+0+"deg)";
+	   		}
+
+	    	else if(crawler.steering == -1 ){
+				document.getElementById("fl").style.transform = "rotate("+-20+"deg)";
+				document.getElementById("fr").style.transform = "rotate("+-20+"deg)";
+		    }
+	    	oldSteering = crawler.steering;
+		}
+	}
+
+
+	function changeBrakingStatus() {
+		/*
+			Update displayed breaking status of crawler.
+		*/
+		brakeMessage = $('#brake-message');
+		brakeCog = $('#brake-cog > i');
+
+		if(crawler.brake < 10){
+				oldSonarState = 0;
+				brakeMessage.text = "Brake Activated.";
+				brakeCog.toggleClass();
+		}
+
+		else if(crawler.brake > 10 && oldSonarState == 0){
+				oldSonarState = 1;
+				brakeMessage.text = "Moving."
+				brakeCog.toggleClass();
+		}
+	}
+
+
+	function changeSonarStatus() {
+		/*
+			Change sonar string displayed on frontend.
+		*/
+		$('#sonar-title').text = crawler.sonar
+	}
+
+
+	function changeWheelStatus(wheel, status){
+		/*
+				Changes wheel display corresponsding to status.
+
+				wheel - String, wheel identifier. ex. "fl"
+				status - Int, current status of the wheel. ex. 1
+		*/
+	    if(status == 1){
+	      $(wheel).animate({ backgroundColor: wheelColorGood, },{ duration: 100, queue: false });
+	   	}
+	   	else{
+	    	$(wheel).animate({ backgroundColor: wheelColorSlipping, },{ duration: 100, queue: false });
 	    }
-    	old = servo;
 	}
 
-}
+	function changeWheelStatusFL() {
+		changeWheelStatus("fl", crawler.wheels.fl);
+	}
 
+	function changeWheelStatusFR() {
+		changeWheelStatus("fr", crawler.wheels.fr);
+	}
 
-// Wheel animation
-var wheel_update_frequency = 0.5
-var wheelUpdateFL = setInterval(changeWheelStatus("fl", FLResponse), 1/wheel_update_frequency);
-var wheelUpdateFR = setInterval(changeWheelStatus("fr", FLResponse), 1/wheel_update_frequency);
-var wheelUpdateRL = setInterval(changeWheelStatus("rl", FLResponse), 1/wheel_update_frequency);
-var wheelUpdateRR = setInterval(changeWheelStatus("rr", FLResponse), 1/wheel_update_frequency);
+	function changeWheelStatusRL() {
+		changeWheelStatus("rl", crawler.wheels.rl);
+	}
 
-function changeWheelStatus(wheel, status){
-	/*
-		Changes wheel display corresponsding to status.
-			wheel - String, wheel identifier. ex. "fl"
-			status - Int, current status of the wheel. ex. 1
-	*/
-		var good = "#98bf21";
-		var bad =  "#d9534f";
+	function changeWheelStatusRR() {
+		changeWheelStatus("rr", crawler.wheels.rr);
+	}
 
-    var div = $(wheel);
-    	if(status == 1){
-        div.animate({ backgroundColor: good, },{ duration: 100, queue: false });
-   		}
-   		else{
-        div.animate({ backgroundColor: bad, },{ duration: 100, queue: false });
-        }
-}
-
-
-
-
-
-// Sonar Animation
-var sonarUpdateFrequency = 0.5
-var sonarID = "sonarStatus"
-var sonarUpdate = setInterval(sonarFunction(sonarID), 1/sonarUpdateFrequency);
-function sonarFunction() {
-	/*
-		Change sonar string displayed on frontend.
-	*/
-    document.getElementById(sonarID).innerHTML = sonarResponse.toString();
-
-}
+});
