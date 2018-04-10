8fb60114b4e666a97b4c266d1ddeb5b03d41210d
Steven Lagrange <slagrang@ualberta.ca>
2018-04-10 14:15:24 -0600

    Added py

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+app_port = 3000
+
+status = {
+    "crawler": {
+        "connected": 0,
+        "message": "Crawler not connected.",
+        "servo": 0,
+        "brake": 0,
+        "sonar": 0,
+        "wheels": {
+          "fl": 0,
+          "fr": 0,
+          "rl": 0,
+          "rr": 0
+        }
+    }
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/')
+def api_home():
+    return render_template('api.html')
+
+@app.route('/api/status/')
+def api_status():
+    return jsonify(status)
+
+@app.route('/api/update/')
+def api_update():
+    return 200
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=app_port)
