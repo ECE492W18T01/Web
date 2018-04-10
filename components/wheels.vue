bde8017d64360fafb3c83be625a68a5e2e4e679f
Steven Lagrange <slagrang@ualberta.ca>
2018-04-10 14:47:41 -0600

    Removed node server

--- components/wheels.vue
+++
@@ -1,5 +0,0 @@
-Vue.component('wheels', {
-  props: ['data'],
-  template: '<div>fr:{{ data.fr }}, fl: {{ data.fl }}, rr: {{ data.rr }}, rl: {{ data.rl }}</div>\
-  <canvas id="wheel-canvas"></canvas>'
-})
